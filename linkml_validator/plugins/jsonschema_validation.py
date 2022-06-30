95c62b518e79bd055c4187e79ddfb398f67f5e50
deepakunni3 <deepak.unni3@gmail.com>
2022-06-30 11:53:54 +0200

    Add ability to provide generator specific args to jsonschema_validation plugin

--- linkml_validator/plugins/jsonschema_validation.py
+++ linkml_validator/plugins/jsonschema_validation.py
@@ -13,16 +13,19 @@ class JsonSchemaValidationPlugin(BasePlugin):
 
     Args:
         schema: Path or URL to schema YAML
+        jsonschema_generator: A generator to use for generating the JSONSchema
+        generator_args: Arguments to instantiate the generator specified in `jsonschema_generator`
         kwargs: Additional arguments that are used to instantiate the plugin
 
     """
 
     NAME = "JsonSchemaValidationPlugin"
 
-    def __init__(self, schema: str, jsonschema_generator: Generator = JsonSchemaGenerator, **kwargs) -> None:
+    def __init__(self, schema: str, jsonschema_generator: Generator = JsonSchemaGenerator, generator_args: Dict = None, **kwargs) -> None:
         super().__init__(schema)
         self.python_module = get_python_module(schema)
         self.jsonschema_generator = jsonschema_generator
+        self.generator_args = generator_args if generator_args else {}
 
     def process(self, obj: Dict, **kwargs) -> ValidationResult:
         """
@@ -42,7 +45,12 @@ class JsonSchemaValidationPlugin(BasePlugin):
         msg = None
         valid = True
         py_target_class = self.python_module.__dict__[target_class]
-        jsonschema_obj = get_jsonschema(self.schema, py_target_class, generator=self.jsonschema_generator)
+        jsonschema_obj = get_jsonschema(
+            schema=self.schema,
+            py_target_class=py_target_class,
+            generator=self.jsonschema_generator,
+            **self.generator_args
+        )
         validator = jsonschema.Draft7Validator(jsonschema_obj)
         errors = [x for x in validator.iter_errors(obj)]
         result = ValidationResult(
