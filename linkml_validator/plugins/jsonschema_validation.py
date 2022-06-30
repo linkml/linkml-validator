d07d1d9a8be4cd04d37358d04874f64908bbf39f
deepakunni3 <deepak.unni3@gmail.com>
2022-06-30 15:35:22 +0200

    Capture all errors and suberrors during jsonschema validation

--- linkml_validator/plugins/jsonschema_validation.py
+++ linkml_validator/plugins/jsonschema_validation.py
@@ -42,7 +42,6 @@ class JsonSchemaValidationPlugin(BasePlugin):
         if "target_class" not in kwargs:
             raise Exception("Need `target_class` argument")
         target_class = kwargs["target_class"]
-        msg = None
         valid = True
         py_target_class = self.python_module.__dict__[target_class]
         jsonschema_obj = get_jsonschema(
@@ -54,18 +53,26 @@ class JsonSchemaValidationPlugin(BasePlugin):
         validator = jsonschema.Draft7Validator(jsonschema_obj)
         errors = [x for x in validator.iter_errors(obj)]
         result = ValidationResult(
-            plugin_name=self.NAME, valid=valid, validation_messages=[]
+            plugin_name=self.NAME,
+            valid=valid,
+            validation_messages=[]
         )
         if errors:
             valid = result.valid = False
             for error in errors:
-                msg = error.message
-                field = ".".join(map(str, error.relative_path)) if error.relative_path else None
-                validation_message = ValidationMessage(
+                outer_validation_message = ValidationMessage(
                     severity=SeverityEnum.error.value,
-                    message=msg,
-                    field=field,
-                    value=error.instance
+                    message=error.message,
+                    field = ".".join(map(str, error.absolute_path)) if error.absolute_path else None,
+                    value=error.instance if not isinstance(error.instance, dict) else None
                 )
-                result.validation_messages.append(validation_message)
+                for suberror in sorted(error.context, key=lambda e: e.schema_path):
+                    inner_validation_message = ValidationMessage(
+                        severity=SeverityEnum.error.value,
+                        message=suberror.message,
+                        field = ".".join(map(str, suberror.absolute_path)) if suberror.absolute_path else outer_validation_message.field,
+                        value=suberror.instance if not isinstance(suberror.instance, dict) else None
+                    )
+                    result.validation_messages.append(inner_validation_message)
+                result.validation_messages.append(outer_validation_message)
         return result
