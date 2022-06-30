0bfce74ec1ab65f9cab67388069cb828ae8da791
Deepak <deepakunni3@users.noreply.github.com>
2022-06-30 15:44:46 +0200

    Add ability to provide generator specific args to jsonschema_validation plugin (#12)
    * Add ability to provide generator specific args to jsonschema_validation plugin
    * Capture all errors and suberrors during jsonschema validation
    * Bump version to 0.4.1

--- linkml_validator/plugins/jsonschema_validation.py
+++ linkml_validator/plugins/jsonschema_validation.py
@@ -13,16 +13,19 @@ class JsonSchemaValidationPlugin(BasePlugin):
 
     Args:
         schema: Path or URL to schema YAML
+        jsonschema_generator: A generator to use for generating the JSONSchema
+        generator_args: Arguments to instantiate the generator specified in `jsonschema_generator`
         kwargs: Additional arguments that are used to instantiate the plugin
 
     """
 
     NAME = "JsonSchemaValidationPlugin"
 
-    def __init__(self, schema: str, jsonschema_generator: Generator = JsonSchemaGenerator, **kwargs) -> None:
+    def __init__(self, schema: str, jsonschema_generator: Generator = JsonSchemaGenerator, generator_args: Dict = None, **kwargs) -> None:
         super().__init__(schema)
         self.python_module = get_python_module(schema)
         self.jsonschema_generator = jsonschema_generator
+        self.generator_args = generator_args if generator_args else {}
 
     def process(self, obj: Dict, **kwargs) -> ValidationResult:
         """
@@ -39,25 +42,37 @@ class JsonSchemaValidationPlugin(BasePlugin):
         if "target_class" not in kwargs:
             raise Exception("Need `target_class` argument")
         target_class = kwargs["target_class"]
-        msg = None
         valid = True
         py_target_class = self.python_module.__dict__[target_class]
-        jsonschema_obj = get_jsonschema(self.schema, py_target_class, generator=self.jsonschema_generator)
+        jsonschema_obj = get_jsonschema(
+            schema=self.schema,
+            py_target_class=py_target_class,
+            generator=self.jsonschema_generator,
+            **self.generator_args
+        )
         validator = jsonschema.Draft7Validator(jsonschema_obj)
         errors = [x for x in validator.iter_errors(obj)]
         result = ValidationResult(
-            plugin_name=self.NAME, valid=valid, validation_messages=[]
+            plugin_name=self.NAME,
+            valid=valid,
+            validation_messages=[]
         )
         if errors:
             valid = result.valid = False
             for error in errors:
-                msg = error.message
-                field = ".".join(map(str, error.relative_path)) if error.relative_path else None
-                validation_message = ValidationMessage(
+                outer_validation_message = ValidationMessage(
                     severity=SeverityEnum.error.value,
-                    message=msg,
-                    field=field,
-                    value=error.instance
+                    message=error.message,
+                    field = ".".join(map(str, error.absolute_path)) if error.absolute_path else None,
+                    value=error.instance if not isinstance(error.instance, dict) else None
                 )
-                result.validation_messages.append(validation_message)
+                for suberror in sorted(error.context, key=lambda e: e.schema_path):
+                    inner_validation_message = ValidationMessage(
+                        severity=SeverityEnum.error.value,
+                        message=suberror.message,
+                        field = ".".join(map(str, suberror.absolute_path)) if suberror.absolute_path else outer_validation_message.field,
+                        value=suberror.instance if not isinstance(suberror.instance, dict) else None
+                    )
+                    result.validation_messages.append(inner_validation_message)
+                result.validation_messages.append(outer_validation_message)
         return result
